9a0120817bcfae804cae15f9770d10c2ee9318b6
Ronaldo Paulino Filho <ronaldofilho2@hotmail.com>
2020-08-31 23:39:34 -0300

    Cliente e Endereco

--- src/main/java/com/aprendendospring/cursospring/CursospringApplication.java
+++ src/main/java/com/aprendendospring/cursospring/CursospringApplication.java
@@ -1,13 +1,7 @@
 package com.aprendendospring.cursospring;
 
-import com.aprendendospring.cursospring.domain.Categoria;
-import com.aprendendospring.cursospring.domain.Cidade;
-import com.aprendendospring.cursospring.domain.Estado;
-import com.aprendendospring.cursospring.domain.Produto;
-import com.aprendendospring.cursospring.repositories.CategoriaRepository;
-import com.aprendendospring.cursospring.repositories.CidadeRepository;
-import com.aprendendospring.cursospring.repositories.EstadoRepository;
-import com.aprendendospring.cursospring.repositories.ProdutoRepository;
+import com.aprendendospring.cursospring.domain.*;
+import com.aprendendospring.cursospring.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -32,6 +26,11 @@ public class CursospringApplication implements CommandLineRunner {
     private CidadeRepository cidadeRepository;
     @Autowired
     private EstadoRepository estadoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
 
 
 	public static void main(String[] args) {
@@ -68,6 +67,15 @@ public class CursospringApplication implements CommandLineRunner {
 
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012",cli1, c2);
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
 
 	}
 
---
+++ src/main/java/com/aprendendospring/cursospring/repositories/ClienteRepository.java
@@ -0,0 +1,14 @@
+package com.aprendendospring.cursospring.repositories;
+
+import com.aprendendospring.cursospring.domain.Cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository <Cliente, Integer> {
+
+
+
+    }
+
