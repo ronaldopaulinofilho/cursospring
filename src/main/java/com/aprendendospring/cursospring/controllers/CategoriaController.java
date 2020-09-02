7fd9ccc35c73714f5bf263ded845c41a47bb33a1
Ronaldo Paulino Filho <ronaldofilho2@hotmail.com>
2020-09-02 16:23:26 -0300

    Put

--- src/main/java/com/aprendendospring/cursospring/controllers/CategoriaController.java
+++ src/main/java/com/aprendendospring/cursospring/controllers/CategoriaController.java
@@ -17,9 +17,9 @@ public class CategoriaController {
     private CategoriaService service;
 
     @RequestMapping(value = "/{id}" , method = RequestMethod.GET)
-    public ResponseEntity <?> find (@PathVariable Integer id){
+    public ResponseEntity <Categoria> find (@PathVariable Integer id){
 
-    Categoria obj = service.buscar(id);
+    Categoria obj = service.find(id);
     return ResponseEntity.ok().body(obj);
 
     }
@@ -28,5 +28,12 @@ public class CategoriaController {
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void>update(@RequestBody Categoria obj, @PathVariable Integer id){
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+
     }
 }
