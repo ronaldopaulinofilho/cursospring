b3a797dcbdb91afd789e12da6c891b6ca599dedb
Ronaldo Paulino Filho <ronaldofilho2@hotmail.com>
2020-09-02 18:50:35 -0300

    Page

--- src/main/java/com/aprendendospring/cursospring/services/CategoriaService.java
+++ src/main/java/com/aprendendospring/cursospring/services/CategoriaService.java
@@ -6,6 +6,9 @@ import com.aprendendospring.cursospring.services.exceptions.DataIntegrityExcepti
 import com.aprendendospring.cursospring.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 
@@ -42,6 +45,10 @@ public class CategoriaService {
   public List<Categoria> findAll(){
         return repo.findAll();
   }
+        public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
+    }
 }
 
